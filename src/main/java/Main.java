0f0afd6e5ff3df71a8d541c9795a52bc513d0c40
JarssS8 <adriancgs@gmail.com>
2021-07-22 14:22:31 +0200

    Complete

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,41 @@
-import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.*;
+import org.eclipse.jgit.internal.storage.file.FileRepository;
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.transport.URIish;
+import org.eclipse.jgit.transport.UsernamePasswordCredentialsProvider;
 
-import java.io.File;
 import java.io.IOException;
 
 public class Main {
-    public static void main(String[] params) throws IOException {
-        System.out.println("Hola");
-        Git git = Git.open(new File("E:/Programming/JGit/.git/"));
+    public static void main(String[] params) {
+        try {
+
+            // Initial values
+            Repository localRepo = new FileRepository("PATH-TO-.git-folder");
+            Git git = new Git(localRepo);
+            // Add
+            AddCommand addCommand = git.add();
+            addCommand.addFilepattern("FILE-PATH").call();
+
+            // Commit
+            CommitCommand commit = git.commit();
+            commit.setMessage("COMMIT-MESSAGE").call();
+
+            // Add remote repo:
+            RemoteAddCommand remoteAddCommand = git.remoteAdd();
+            remoteAddCommand.setName("origin");
+            remoteAddCommand.setUri(new URIish("REPO-URL"));
+            // you can add more settings here if needed
+            remoteAddCommand.call();
+
+            // Push to remote:
+            PushCommand pushCommand = git.push();
+            // CREDENTIALS NEVER SHOULD BE SHOWED IN CODE
+            // you can add more settings here if needed
+            pushCommand.setCredentialsProvider(new UsernamePasswordCredentialsProvider("username", "password"));
+            pushCommand.call();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
